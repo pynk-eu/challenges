6d78dcac7d8f72b03f0106cdb895544b34a3ad91
priyanka gupta <pynk1992@gmail.com>
2023-08-23 16:16:07 +0200

    challenge not completed

--- javascript/JS-Forms/js-forms_checkbox-input/js/index.js
+++ javascript/JS-Forms/js-forms_checkbox-input/js/index.js
@@ -6,6 +6,7 @@ const tosError = document.querySelector('[data-js="tos-error"]');
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
+hideTosError();
 
 function showTosError() {
   tosError.removeAttribute("hidden");
@@ -13,11 +14,19 @@ function showTosError() {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formTos = event.target.elements.tos.checked;
+  console.log(formTos);
+
+  if (formTos === true) {
+    hideTosError();
+    alert("Form submitted");
+  } else {
+    showTosError();
+  }
 
   // --v-- write your code here --v--
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
 });
