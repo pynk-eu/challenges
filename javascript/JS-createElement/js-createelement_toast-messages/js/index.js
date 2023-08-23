b1ff13a04d37fdd926ba854c5c0b760512331c73
priyanka gupta <pynk1992@gmail.com>
2023-08-23 17:25:32 +0200

    challenge toast done

--- javascript/JS-createElement/js-createelement_toast-messages/js/index.js
+++ javascript/JS-createElement/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,14 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const toastMsg = document.createElement("li");
+  toastMsg.textContent = "I am new toast message";
+  toastContainer.append(toastMsg);
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = " ";
+
   // Exercise: Clear the stack of toast messages
 });
