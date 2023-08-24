76a910e95d2bf6cac1eca57b7750578cea04d231
priyanka gupta <pynk1992@gmail.com>
2023-08-24 10:32:40 +0200

    challenge social media

--- javascript/JS-createElement/js-createelement_social-media-post/js/index.js
+++ javascript/JS-createElement/js-createelement_social-media-post/js/index.js
@@ -8,5 +8,19 @@ function handleLikeButtonClick(event) {
 const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
+likeButton.addEventListener("mouseover", () => {});
+
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+document.body.append(article);
+const article = document.createElement("article");
+const p = document.createElement("p");
+const footer = document.createElement("footer");
+const span = document.createElement("span");
+const button = document.createElement("button");
+
+article.append(p);
+article.append(footer);
+footer.append(span);
+footer.append(button);
