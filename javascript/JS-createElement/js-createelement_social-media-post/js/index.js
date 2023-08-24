c53aab6a6ca470a2305a819ca3d01601c685ee23
priyanka gupta <pynk1992@gmail.com>
2023-08-24 17:13:06 +0200

    challenge social media post done

--- javascript/JS-createElement/js-createelement_social-media-post/js/index.js
+++ javascript/JS-createElement/js-createelement_social-media-post/js/index.js
@@ -8,19 +8,30 @@ function handleLikeButtonClick(event) {
 const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
-likeButton.addEventListener("mouseover", () => {});
-
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
 
-document.body.append(article);
-const article = document.createElement("article");
-const p = document.createElement("p");
-const footer = document.createElement("footer");
-const span = document.createElement("span");
+const bodyElement = document.querySelector("body");
+const articleElement = document.createElement("article");
+const pElement = document.createElement("p");
+const footerElement = document.createElement("footer");
+const spanElement = document.createElement("span");
 const button = document.createElement("button");
 
-article.append(p);
-article.append(footer);
-footer.append(span);
-footer.append(button);
+articleElement.classList.add("post");
+pElement.classList.add("post__content");
+pElement.textContent =
+  " Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+footerElement.classList.add("post__footer");
+spanElement.classList.add("post__username");
+spanElement.textContent = "@username";
+button.classList.add("post__button");
+button.textContent = " ♥ Like";
+
+bodyElement.append(articleElement);
+articleElement.append(pElement);
+articleElement.append(footerElement);
+footerElement.append(spanElement);
+footerElement.append(button);
+
+button.addEventListener("click", handleLikeButtonClick);
