605d8d02837ce0df1e20fd37deced0714c712fc7
priyanka gupta <pynk1992@gmail.com>
2023-08-31 23:33:17 +0200

    challenge flags done

--- javascript/JS-ArrayMethods-2/js-array-methods-2_fun-with-flags/index.js
+++ javascript/JS-ArrayMethods-2/js-array-methods-2_fun-with-flags/index.js
@@ -7,12 +7,16 @@ const queryInput = document.querySelector('[data-js="query-input"]');
 queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
-  const searchString = event.target.value;
+  const searchString = event.target.value.toLowerCase();
 
-  const foundCountry = null;
+  const foundCountries = countries.filter((country) => {
+    return country.name.toLowerCase().startsWith(searchString);
+  });
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
+  if (foundCountries) {
+    foundCountries.forEach((country) => {
+      const countryElement = Country(country);
+      container.append(countryElement);
+    });
   }
 });
