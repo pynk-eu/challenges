996d048ce3b9b14ee52f9478ba10a7fcfb3a7018
priyanka gupta <pynk1992@gmail.com>
2023-08-31 22:50:26 +0200

    challenge news flash done

--- javascript/JS-ArrayMethods-2/js-array-methods-2_news-flash/index.js
+++ javascript/JS-ArrayMethods-2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,14 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+const filteredNews = news.filter((card) => {
+  return card.categories.includes("politics");
 });
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.sort(
+  (card1, card2) => card1.body.length - card2.body.length
+);
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
