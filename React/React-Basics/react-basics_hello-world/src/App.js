1ed168cc6fb254802bb63d2d43f3ca3c5f961193
priyanka gupta <pynk1992@gmail.com>
2023-09-11 12:03:30 +0200

    challenge hello world done

--- React/React-Basics/react-basics_hello-world/src/App.js
+++ React/React-Basics/react-basics_hello-world/src/App.js
@@ -1,6 +1,7 @@
+import HelloWorld from "./Component/Hello";
 import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Hello world !</h1>;
+  return <HelloWorld />;
 }
