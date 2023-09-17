c9acd7456dc910061173dcfe542e7bfaf3fede62
priyanka gupta <pynk1992@gmail.com>
2023-09-17 18:36:00 +0200

    challenge iss tracker done.

--- React/React-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.js
+++ React/React-effects-and-fetch/react-effects-and-fetch_iss-tracker/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Controls from "./components/Controls";
 import Map from "./components/Map";
 import "./styles.css";
@@ -11,8 +11,18 @@ export default function App() {
     latitude: 0,
   });
 
-  async function getISSCoords() {}
+  async function getISSCoords() {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setCoords(data);
+  }
 
+  useEffect(() => {
+    const IntervalID = setInterval(getISSCoords, 5000);
+    console.log(IntervalID);
+    clearInterval(IntervalID);
+  }, []);
+  //
   return (
     <main>
       <Map longitude={coords.longitude} latitude={coords.latitude} />
