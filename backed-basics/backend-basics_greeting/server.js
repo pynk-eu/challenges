11975f9317aea644753baacac6904312556985a1
priyanka gupta <pynk1992@gmail.com>
2023-10-05 15:27:26 +0200

    challenge greeting basics done

--- backed-basics/backend-basics_greeting/server.js
+++ backed-basics/backend-basics_greeting/server.js
@@ -2,5 +2,5 @@ import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
   response.statusCode = 200;
-  response.end("Hello, [Priyanka]!");
+  response.end("Hello, ");
 });
