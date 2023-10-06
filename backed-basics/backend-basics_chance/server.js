c427df59d529b55724f762855145c8beeeb2b5b7
priyanka gupta <pynk1992@gmail.com>
2023-10-06 11:32:59 +0200

    challenge backend chance

--- backed-basics/backend-basics_chance/server.js
+++ backed-basics/backend-basics_chance/server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
 // export const server = …
+
+const chance = new Chance();
+
+export const server = createServer((request, response) => {
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+
+  const responseString = `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`;
+
+  responseString.setHeader("Content-Type", "text/plain");
+  response.statusCode = 200;
+
+  response.end(responseString);
+});
